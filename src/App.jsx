948b241f6c88c44e3a7ec827e4c6511a8bfd3552
Yasmin Silva <alunoyasminsilva07@gmail.com>
2024-06-09 22:21:09 +0000

    feat: instalação e implantação do recact-router-dom e do styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-export default function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+
+import { Home } from "./pages/Home"
+import { theme } from "./styles/theme"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    }
+])
+
+export function App() {
   return (
-    <div>App</div>
+    <ThemeProvider theme ={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
